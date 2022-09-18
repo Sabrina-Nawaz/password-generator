f7152f3900624c8dc1c3fbc2bbbd0fc65ec3c715
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-09-18 11:51:54 -0400

    Deleted code in JS script, created variables and if else statements

---
+++ script.js
@@ -0,0 +1,67 @@
+// Create character set  to randomly generate password 
+var lowerCaseStr = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericsStr = "0123456789";
+var specialStr = "!@#$%^&*()~";
+
+var lowerCaseArr = lowerCaseStr.split('');
+var upperCaseArr = upperCaseStr.split('');
+var numbersArr = numericsStr.split('');
+var specialsArr = specialStr.split('');
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  // Logic 
+  // 1. Create 1 prompt for password criteria 
+  // 2. Use prompt variable and at => 8 characters, but <128 characters
+  var pLength = prompt('length of password')
+
+  if (pLength >= 8 && pLength < 128) {
+    alert('Password matches the length criteria!')
+  } else {
+    alert('Password does not match the length criteria!')
+    return null;
+  } 
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  var lCase = prompt('Include lowercase letters');
+
+
+  // 4. Input is validated and one type is selected 
+  // 5. Will need to use a for loop 
+  // 6. Create a function for Character Set
+  // 7. Use an alert prompt
+
+
+  // WHEN I answer each prompt
+  // THEN my input should be validated and at least one character type should be selected
+  // WHEN all prompts are answered
+  // THEN a password is generated that matches the selected criteria
+  // WHEN the password is generated
+  // THEN the password is either displayed in an alert or written to the page
+
+  return 'Sabrinas password'
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
