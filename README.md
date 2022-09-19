05e5cdd184083eef005c6a3114c82b2c74377027
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-09-19 16:57:36 -0400

    Shortened if statement when criterias are true, changed operator in pLength and edited the README

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # password-generator
 
+## Description
+
+Creating passwords online can seem like a headache, but for the protection of your safety and security online, the Password Generator was created as a web application in which you can generate safe and secure passwords with multiple options/criterias offered. 
+
+## Usage
+
+When you click on the "Generate Password" button, you will be met with multiple prompts and confirmations for the criterias you select for your password. It will then generate a random password between 8 - 128 characters, depending on the length you have entered. 
+
+## Credits
+
+Refactored by: Sabrina Nawaz - www.github.com/sabrina-nawaz
+
+W3 Schools: https://www.w3schools.com/html/html5_semantic_elements.asp
+
+MDN Web Docs:  https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements
+
+## License
+
+Found at the bottom of the website. 
+
 https://www.alt-codes.net/check-mark-symbols.php
 
 https://developer.mozilla.org/en-US/docs/Web/API/Node/firstChild
@@ -11,3 +31,6 @@ https://www.w3schools.com/js/js_loop_for.asp
 https://www.javatpoint.com/javascript-prompt-dialog-box
 
 https://www.w3schools.com/jsref/jsref_substring.asp
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create character set
+// Variables below stored in the global scope hold the password criterias to be used.
 var lowerCaseStr = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericsStr = "0123456789";
@@ -13,58 +13,46 @@ var specialsArr = specialStr.split('');
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Logic 
-  // 1. Create 1 prompt for password criteria 
-  // 2. Use prompt variable and at => 8 characters, but <128 characters
+// Variables below are created in the local scope of the function to create empty arrays for data to be pushed into and defines the parameters for the length of the password. 
   var passChars = [];
-  var finalPasword = [];
+  var finalPassword = [];
   var pLength = prompt('length of password');
-
-  if (pLength < 8 && pLength > 128) {
+// The prompt below is to ensure that the user meets the requirements for the password length. If the password is below 8 characters or over 128 characters, then the prompt pops up to inform them the password does not match the criteria. 
+  if (pLength < 8 || pLength > 128) {
     alert('Password does not match the length criteria!')
     return null;
   }
-  // WHEN asked for character types to include in the password
-  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  // Variables below store prompts for the criterias to be selected for a password: lowercase, uppercase, numeric and/or special characters. 
   var lCase = confirm('Do you want to include lowercase letters?');
   var uCase = confirm('Do you want to include uppercase letters?');
   var numbers = confirm('Do you want to include numbers?');
   var specialChar = confirm('Do you want to include special characters?');
 
-  // 4. Input is validated and one type is selected 
+  // When the criterias are all false, an alert pops up to indicate at least one option should be selected and validated.  
   if (lCase === false && uCase === false && numbers === false && specialChar === false) {
     alert('At least one character type should be selected');
     return null;
   }
-
-
-  if (lCase === true) {
+// When the criterias for the password are true and selected, the concat function joins two or more arrays to generate a string.
+  if (lCase === true && uCase === true && numbers === true && specialChar === true) {
     passChars = passChars.concat(lowerCaseArr)
-  }
-  if (uCase === true) {
     passChars = passChars.concat(upperCaseArr)
-  }
-  if (numbers === true) {
     passChars = passChars.concat(numbersArr)
-  }
-  if (specialChar === true) {
     passChars = passChars.concat(specialsArr)
   }
-
+// The length of the password is looped over to pick out parts of each criteria and create a random string under the variable randomChar, which is then pushed into the finalPassword array.
   for (var i = 0; i < pLength; i++) {
-    var randomChar =randomIndex(passChars)
-      finalPasword.push(randomChar)
+    var randomChar = randomIndex(passChars)
+      finalPassword.push(randomChar)
   }
 
-  return finalPasword.join('');
+  return finalPassword.join('');
 }
-
+// randomIndex uses the Math.random() function to round down with Math.floor and the returns a number that is greater than or equal to 0.0 and less than 1.0. It is multiplied with the length of the array.
 function randomIndex(arr){
 
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
